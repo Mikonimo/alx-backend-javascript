35d9ebff262f68d416eb2283304813f7c370b359
Mark Manani <mikonani413@gmail.com>
2024-10-31 00:38:55 +0300

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,12 @@
 export function handleResponseFromAPI(promise) {
-    return
+    return promise.then(() => {
+        console.log('Got a response from the API');
+        return {
+            status: 200,
+            body: 'success',
+        };
+    }).catch(() => {
+        console.log('Got a response from the API');
+        return new Error();
+    });
 }
