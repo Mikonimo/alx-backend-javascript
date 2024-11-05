cd6ad08b28bc26e9341c708b59329cdb89247851
Mark Manani <mikonani413@gmail.com>
2024-11-05 23:23:55 +0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,11 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photoResponse, userResponse] = results;
-      console.log(`${photoResponse.body} ${userResponse.firstName}
-        ${userResponse.lastName}`);
-    }).catch(() => {
-      console.log('Signup system offline');
-    });
+  .then((results) => {
+    console.log(`${results[0]}.body ${results[1]}.firstName} "${results[1].lastName}`);
+  })
+  .catch(() => console.log('Signup system offline'));
 }
