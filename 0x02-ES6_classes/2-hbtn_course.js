cc544b1e9d1cfd541b167820b911de9519e3cf85
Mark Manani <mikonani413@gmail.com>
2024-11-09 15:23:06 +0300

    task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,62 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (name.hasOwnProperty(String)) {
-        this._name = name;
+    /* Validate the types of input parameters */
+    this._validateName(name);
+    this._validateLength(length);
+    this._validateStudents(students);
+
+    /* Initialize attributes */
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  /* Getter and Setter for name */
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._validateName(value);
+    this._name = value;
+  }
+
+  /* Getter and Setter for length */
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._validateLength(value);
+    this._length = value;
+  }
+
+  /* Getter and Setter for students */
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._validateStudents(value);
+    this._students = value;
+  }
+
+  /* Validation Methods */
+  _validateName(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    if (length.hasOwnProperty(Number)) {
-        this._length = length;
+  }
+
+  _validateLength(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    if (students.hasOwnProperty(Array)) {
-        this._students = students;
+  }
+
+  _validateStudents(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
-s
