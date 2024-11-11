f7884837283741e4beb0ee7880ac0fe9ac46daeb
Mark Manani <mikonani413@gmail.com>
2024-11-12 00:30:50 +0300

    Task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
 export default function getListStudentds() {
-  const obj = [
+  return [
     { id: 1, firstName: 'Guillame', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisc' },
   ];
 
-  return obj;
 }
