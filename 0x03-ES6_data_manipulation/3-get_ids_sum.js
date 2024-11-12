175829eb8d8cf5364abebdb28d156152b0a94827
Mark Manani <mikonani413@gmail.com>
2024-11-12 09:44:17 +0300

    task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,9 @@
-export default function getStudentIdsSum() {
-    
+export default function getStudentIdsSum(students) {
+  if (Array.isArray(students)) {
+    return students.reduce(
+      (prevStudent, currStudent) => prevStudent.id || prevStudent + currStudent.id,
+      0,
+    );
+  }
+  return 0;
 }
