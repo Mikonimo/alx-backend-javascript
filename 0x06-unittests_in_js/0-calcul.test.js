4722339db25e3d49a98e0e84af4ad5ca18e40a3f
mikonimo <mikonani413@gmail.com>
2024-11-28 10:29:16 +0300

    Task 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function () {
+    it('should return 4 when inputs are 1 and 3', function () {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return 5 when inputs are 1 and 3.7', function () {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return 5 when inputs are 1.2 and 3.7', function () {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return 6 when inputs are 1.5 and 3.7', function () {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('should handle negative numbers', function () {
+        assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
+    });
+
+    it('should handle a mix of negative and positive numbers', function () {
+        assert.strictEqual(calculateNumber(-1.4, 3.6), 3);
+    });
+
+    it('should handle zero values', function () {
+        assert.strictEqual(calculateNumber(0, 0), 0);
+    });
+});
