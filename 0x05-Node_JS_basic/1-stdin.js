5db8b0bab65cf0f6f1e4fd5cc2f673e105d7cb41
mikonimo <mikonani413@gmail.com>
2024-11-30 02:55:34 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,16 @@
 // Display the initial message
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  // Trim the input to remove extra whitespace or newlines
-  const input = data.toString().trim();
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
 
-  // Display the user's input
-  console.log(`Your name is: ${input}`);
-
-  // Close the stdin stream
-  process.stdin.end();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
 // Handle the end of the input stream
 process.stdin.on('end', () => {
   // Display the closing message
-  console.log("This important software is now closing");
+  process.stdout.write('This important software is now closing\n');
 });
