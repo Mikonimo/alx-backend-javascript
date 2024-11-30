79fc95a8e90fd7f70df03851ed8d8b1fbf65bdbd
mikonimo <mikonani413@gmail.com>
2024-11-30 05:30:05 +0300

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,31 +1,36 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-let server;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-const BASE_URL = 'http://localhost:7865';
-
-describe('Index page', () => {
-    before((done) => {
-        server = app.listen(7865, () => done()); // Start the server for testing
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
+  });
 
-    after(() => {
-        server.close(); // Close the server after all tests
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
     });
+  });
 
-    it('should return status code 200', (done) => {
-        request.get(`${BASE_URL}/`, (err, res, body) => {
-            expect(res.statusCode).to.equal(200);
-            done();
-        });
+  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
     });
+  });
 
-    it('should return "Welcome to the payment system"', (done) => {
-        request.get(`${BASE_URL}/`, (err, res, body) => {
-            expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
     });
+  });
 });
